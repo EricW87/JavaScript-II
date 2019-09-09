fa02a870cdf3259357ed64cb4d8d4d404b7c6f71
Eric Wood <epwood1@gmail.com>
2019-09-08 18:15:05 -0700

    Finished closure.js stretch

--- assignments/closure.js
+++ assignments/closure.js
@@ -32,24 +32,77 @@ holiday_pay();
 
 // ==== Challenge 2: Implement a "counter maker" function ====
 const counterMaker = () => {
-  // IMPLEMENTATION OF counterMaker:
-  // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
-  // 2- Declare a function `counter`. It should increment and return `count`.
-  //      NOTE: This `counter` function, being nested inside `counterMaker`,
-  //      "closes over" the `count` variable. It can "see" it in the parent scope!
-  // 3- Return the `counter` function.
-};
-// Example usage: const myCounter = counterMaker();
-// myCounter(); // 1
-// myCounter(); // 2
+	let count = 0;
+	
+	function counter() {
+		count++;
+		//console.log(count);
+	}
 
+	return counter; 
+};
+
+const myCounter = counterMaker();
+myCounter(); // 1
+myCounter(); // 2
 // ==== Challenge 3: Make `counterMaker` more sophisticated ====
 // It should have a `limit` parameter. Any counters we make with `counterMaker`
 // will refuse to go over the limit, and start back at 1.
 
+const counterMaker2 = (initial_count, limit) => {
+	let count = initial_count;
+	
+	function counter() {
+		count++;
+
+		if(count > limit)
+		{
+			count = initial_count;
+			count++;
+		}
+
+		//console.log(count);
+	}
+
+	return counter; 
+};
+
+const myBetterCounter = counterMaker2(0, 100);
+
+for(i = 0; i < 200; i++)
+	myBetterCounter();
 // ==== Challenge 4: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+	let count = 0;
+
+	const incrementAndDecrement = {
+		increment:  function() {
+			count++;
+		},
+
+		decrement: function() {
+			count--;
+		},
+
+		display: () => console.log(count)
+
+	}
+
+	return incrementAndDecrement; 
 };
+
+const supercounter = counterFactory();
+supercounter.increment();
+supercounter.display();
+supercounter.decrement();
+supercounter.display();
+supercounter.increment();
+supercounter.increment();
+supercounter.increment();
+supercounter.increment();
+supercounter.increment();
+supercounter.increment();
+supercounter.display();
